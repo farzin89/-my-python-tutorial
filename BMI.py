e2c24f77b77a80c69b68f1cc285632ea87d8a9f9
Farzin <86613527+farzin89@users.noreply.github.com>
2021-11-10 17:57:31 +0100

    Add files via upload

---
+++ BMI.py
@@ -0,0 +1,7 @@
+name = input("name:")
+vazn = input("vazn (KG):")
+vazn_float = float (vazn)
+ghad = input("ghad (M):")
+ghad_float = float(ghad)
+BMI = vazn_float/(ghad_float**2)
+print(name,"aziz","bmi shoma:",BMI)
