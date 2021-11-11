c3441762e8c520b65cc0cfe4a37d82f87a562999
Farzin <86613527+farzin89@users.noreply.github.com>
2021-11-11 09:58:19 +0100

    Add files via upload

---
+++ while-if.py
@@ -0,0 +1,13 @@
+
+while True:
+
+    ramze_shab = input("ramz e shab?")
+
+    if ramze_shab == "f":
+        print("mitoni vared shi")
+    elif ramze_shab == "B":
+        print("jasooos")
+    elif ramze_shab == "C":
+        print("befarma")
+    else :
+        print("ghalat")
