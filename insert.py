dad4fc5a37a5ca6966f5c5a42e23f692b08e6a60
Farzin <86613527+farzin89@users.noreply.github.com>
2021-11-14 16:18:36 +0100

    Add files via upload

---
+++ insert.py
@@ -0,0 +1,18 @@
+
+while True:
+
+    mylist = ["A11","A12","Note10","Iphone12pro"]
+
+    new_item_name = input("mobile e jadid vared konid")
+    new_item_index = int(input("Jaygah mobile ra vared konid"))
+
+    mylist.insert(new_item_index-1,new_item_name)
+    print(mylist)
+
+
+
+
+
+
+
+
